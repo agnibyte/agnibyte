0c1e1dab4f079f7d27e809fa0fa446659be025e7
dnyaneshwarshekade <shekade31@gmail.com>
2024-10-03 03:12:59 +0530

    final try

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,15 +2,17 @@
 import { fetchBlogs, fetchBlogBySysId } from "@/lib/blogService";
 import BlogDetailPage from "@/components/Blog/BlogDetailPage";
 
+// This will generate static params for each blog post at build time.
 export async function generateStaticParams() {
   const data = await fetchBlogs(); // Fetch all blogs
   const allBlogs = data.pageBlogPostCollection.items; // Extract the items array
 
   if (!Array.isArray(allBlogs)) {
     console.error("Expected an array, but got:", allBlogs);
-    return []; // Return an empty array or handle the error appropriatelyhjhjhjvbhjvh
+    return []; // Return an empty array or handle the error appropriately
   }
 
+  // Map each blog to a param for the static generation
   return allBlogs.map((blog: { _id: string }) => ({
     params: { id: blog._id } // Generate static params
   }));
@@ -21,7 +23,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   const blogDetails = await fetchBlogBySysId(params.id);
 
   if (!blogDetails) {
-    return <p>Blog post not found.</p>;
+    return <p>Blog post not found.</p>; // Handle missing blog post
   }
 
   return (
