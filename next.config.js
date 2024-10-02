3be2e86db520d6052e8cf1f996a62fbb358fbbf7
dnyaneshwarshekade <shekade31@gmail.com>
2024-10-03 03:15:45 +0530

    update

--- next.config.js
+++ next.config.js
@@ -7,16 +7,17 @@ const nextConfig = {
         protocol: "https",
         hostname: "agni-byte.com",
         port: "",
-        //pathname: "/public/images", // Matches all paths on agni-byte.com
+        // pathname: "/public/images", // Matches all paths on agni-byte.com
       },
       {
         protocol: "https",
         hostname: "images.ctfassets.net",
         port: "",
-        //pathname: "/public/images", // Matches all paths on agni-byte.com
-      },
+        // pathname: "/public/images", // Matches all paths on images.ctfassets.net
+      }
     ],
   },
+  output: 'export', // Enable static export
 };
 
 module.exports = nextConfig;
