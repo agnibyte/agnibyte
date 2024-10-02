0b0de0106e1bb66409a09ac3692a2079a72fbc2b
dnyaneshwarshekade <shekade31@gmail.com>
2024-10-03 02:56:52 +0530

    error fix github deploy

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,26 @@
 import BlogDetailPage from "@/components/Blog/BlogDetailPage";
-import { fetchBlogBySysId } from "@/lib/blogService";
+import { fetchBlogs, fetchBlogBySysId } from "@/lib/blogService";
 import { Metadata } from "next";
 
+// Metadata for SEO
 export const metadata: Metadata = {
   title: 'Blog Details',
   description: 'Read the full blog post',
 };
+
+// Static Params generation for dynamic routes
+export async function generateStaticParams() {
+  const allBlogs = await fetchBlogs(); // Fetch all blogs
+
+  if (!Array.isArray(allBlogs)) {
+    console.error('Expected an array, but got:', allBlogs);
+    return []; // Return an empty array or handle the error appropriately
+  }
+
+  return allBlogs.map((blog: { _id: string }) => ({
+    params: { id: blog._id } // Extract blog IDs for the dynamic route
+  }));
+}
 
 export default async function Page({ params }: { params: { id: string } }) {
   const blogDetails = await fetchBlogBySysId(params.id);
