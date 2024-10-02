ef913692239d82aabf6cbc815ef6e999a5c12b73
suraj <agnibyte@gmail.com>
2024-10-02 12:34:53 +0530

    test

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -7,19 +7,8 @@ export const metadata: Metadata = {
   description: 'Read the full blog post',
 };
 
-async function fetchBlogDetailsData(id: string) {
-  try {
-    const blogData = await fetchBlogBySysId(id);
-
-    return blogData;
-  } catch (error) {
-    console.error('Failed to fetch blog data:', error);
-    return null;
-  }
-}
-
-export default async function BlogPostPage({ params }: { params: { id: string } }) {
-  const blogDetails = await fetchBlogDetailsData(params.id);
+export default async function Page({ params }: { params: { id: string } }) {
+  const blogDetails = await fetchBlogBySysId(params.id);
 
   if (!blogDetails) {
     return <p>Blog post not found.</p>;
