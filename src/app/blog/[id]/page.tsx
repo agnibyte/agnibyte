f3ebd3774c65fb8e0b44c764f03334d731058c38
suraj <agnibyte@gmail.com>
2024-10-06 17:24:50 +0530

    test

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,21 +2,22 @@ import BlogDetailPage from '@/components/Blog/BlogDetailPage';
 import { fetchBlogBySysId, fetchBlogs } from '@/lib/blogService';
 import { Metadata } from 'next';
 
+// Static metadata (can also be dynamic as needed)
 export const metadata: Metadata = {
   title: 'Blog Details - Full Post',
   description: 'Read the full blog post with in-depth insights and analysis.',
-  keywords: ['blog', 'tech', 'insights', 'full post', 'Agnibyte Tech'], // Relevant keywords for SEO
-  robots: 'index, follow', // SEO robots directive
+  keywords: ['blog', 'tech', 'insights', 'full post', 'Agnibyte Tech'],
+  robots: 'index, follow',
   openGraph: {
     title: 'Blog Details - Agnibyte Tech',
     description: 'Explore detailed insights and analysis in our blog post.',
-    url: 'https://www.agni-byte.com/blog-details', // Replace with actual blog URL
+    url: 'https://www.agni-byte.com/blog-details',
     type: 'article',
     locale: 'en_US',
     siteName: 'Agnibyte Tech',
     images: [
       {
-        url: '/images/blog-favicon.png', // Replace with actual image
+        url: '/images/blog-favicon.png',
         alt: 'Blog thumbnail',
         width: 1200,
         height: 630,
@@ -27,24 +28,33 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Blog Details - Agnibyte Tech',
     description: 'Read the full blog post on Agnibyte Tech’s website.',
-    images: 'https://www.agni-byte.com/images/blog-thumbnail.jpg', // Replace with actual image
+    images: 'https://www.agni-byte.com/images/blog-thumbnail.jpg',
   },
   viewport: 'width=device-width, initial-scale=1.0',
 };
 
-const BlogPage = async ({ params }) => {
+// Define static params for all blog posts to be pre-generated
+export async function generateStaticParams() {
+  const [allBlogs] = await Promise.all([fetchBlogs()]);
 
+  return allBlogs.pageBlogPostCollection.items.map((blog: { sys: { id: any; }; }) => ({
+    slug: blog.sys.id,
+  }))
+}
+
+// Blog page component
+const BlogPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
-  let blogDetails = null
+  let blogDetails = null;
 
   try {
-    blogDetails = await fetchBlogBySysId(id) // Fetch job posts
+    blogDetails = await fetchBlogBySysId(id); // Fetch blog post by ID
   } catch (error) {
-    console.error('Error fetching job posts:', error);
-    // Optionally, you can set jobPosts to a default value or show an error message
+    console.error('Error fetching blog post:', error);
   }
+
   if (!blogDetails) {
-    <p>Blog post not found.</p>
+    return <p>Blog post not found.</p>;
   }
 
   return (
@@ -58,5 +68,6 @@ const BlogPage = async ({ params }) => {
       />
     </div>
   );
-}
+};
+
 export default BlogPage;
